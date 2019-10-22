9252c595576a4f992c5b0a1a836d55cbfb71a11d
leeing <lining@growingio.com>
2019-10-22 16:11:10 +0800

    Bug fixed

--- src/components/layout/LayoutSider.jsx
+++ src/components/layout/LayoutSider.jsx
@@ -7,28 +7,28 @@ import logo from '../../logo.svg';
 const { Sider } = Layout;
 
 const LayoutSider = ({ selectedKeys, routes }) => {
-	const [collapsed, setCollapsed] = useState(false);
-	return	(
-		<Sider
-			collapsible
-			collapsed={collapsed}
-			onCollapse={col => setCollapsed(col)}
-			theme="light"
-		>
-			<div className={styles.menuTitle}>
-				<img src={logo} alt="logo"/>
-				<span>React</span>
-			</div>
-			<Menu selectedKeys={selectedKeys} mode="inline">
-				{routes.filter(item => !item.hide).map(item => (
-					<Menu.Item key={item.path}>
-						<Icon type={item.icon} />
-						<Link to={item.path}>{item.title}</Link>
-					</Menu.Item>
-				))}
-			</Menu>
-		</Sider>
-	);
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={col => setCollapsed(col)}
+            theme="light"
+        >
+            <div className={styles.menuTitle}>
+                <img className={styles.menuLogo} src={logo} alt="logo" />
+                <span>React</span>
+            </div>
+            <Menu selectedKeys={selectedKeys} mode="inline">
+                {routes.filter(item => !item.hide).map(item => (
+                    <Menu.Item key={item.path}>
+                        <Icon type={item.icon} />
+                        <Link to={item.path}>{item.title}</Link>
+                    </Menu.Item>
+                ))}
+            </Menu>
+        </Sider>
+    );
 };
 
 export default LayoutSider;
