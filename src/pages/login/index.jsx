59360b29315b755fd86ed4992e50e0224ceb0eac
leeing <lining@growingio.com>
2020-03-16 12:43:20 +0800

    Test scrollListener

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 import LoginForm from '@components/LoginForm';
 import logo from '../../logo.svg';
 import styles from './login.module.scss';
+import { createListener } from '@1eeing/scroll-listener';
 
-const Login = () => (
-	<div className={[styles.fullScreen, styles.loginBgBottom].join(' ')}>
-		<div className={[styles.loginBgTop]}>
-            <img className={styles.loginLogo} src={logo} alt="logo"/>
-			<div className={[styles.loginContent]}>
-				<LoginForm />
-			</div>
-		</div>
-	</div>
-)
+const Login = () => {
+  React.useEffect(() => {
+    const listener = createListener({
+      positions: ['main'],
+      actions: [(e, position) => console.log('This is main', position)],
+      requestIdleCallback: true,
+      delayType: 'throttle',
+    });
+
+    listener.start();
+    return () => listener.stop();
+  }, [])
+
+  return (
+    <div className={[styles.fullScreen, styles.loginBgBottom].join(' ')}>
+      <div id='main' className={[styles.loginBgTop]}>
+        <img className={styles.loginLogo} src={logo} alt="logo" />
+        <div className={[styles.loginContent]}>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Login;
